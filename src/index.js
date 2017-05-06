7422ecaded85a32454db00c36e9482d5690d9209
Leonardo Santos <devleeo@outlook.com>
2017-05-06 03:59:37 -0300

    change license to MIT

--- src/index.js
+++ src/index.js
@@ -1,20 +1,9 @@
-/*
- *  Copyright (C) 2017 leonardosnt
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License along
- *  with this program; if not, write to the Free Software Foundation, Inc.,
- *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-*/
+/*!
+ * https://github.com/leonardosnt/java-class-tools
+ * 
+ * Copyright (C) 2017 leonardosnt
+ * Licensed under the MIT License. See LICENSE file in the project root for full license information.
+ */
 
 import JavaClassFileReader from './java-class-reader';
 import JavaClassFileWriter from './java-class-writer';
