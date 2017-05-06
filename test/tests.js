98725106e695bfced114220f0f8ead742c1c3d71
Leonardo Santos <devleeo@outlook.com>
2017-05-06 15:01:07 -0300

    Revert "don't convert the operands to int32" Moved to dev branch, wait to release.
    This reverts commit 9a11202cb9c5fabffad6406e4a4d4f33c909bfc2.

--- test/tests.js
+++ test/tests.js
@@ -46,37 +46,9 @@ describe('instruction parser', () => {
 
       String x = "asdasdsad";
 
-      // Test lookupswitch
       switch (x) {
         case "asdasdsad": return;
         case "asdasdasd": break;
-      }
-
-      // Test tableswitch
-      int j = 23;
-      switch (j) {
-        case 0: System.out.println("0"); break;
-        case 1: System.out.println("1"); break;
-        case 2: System.out.println("2"); break;
-        case 4: System.out.println("4"); break;
-        case 5: System.out.println("5"); break;
-        case 6: System.out.println("6"); break;
-        case 7: System.out.println("7"); break;
-        case 9: System.out.println("9"); break;
-        case 10: System.out.println("10"); break;
-        case 11: System.out.println("11"); break;
-        case 12: System.out.println("12"); break;
-        case 14: System.out.println("14"); break;
-        case 15: System.out.println("15"); break;
-        case 16: System.out.println("16"); break;
-        case 17: System.out.println("17"); break;
-        case 20: System.out.println("20"); break;
-        case 21: System.out.println("21"); break;
-        case 22: System.out.println("22"); break;
-        case 23: System.out.println("23"); break;
-        case 26: System.out.println("26"); break;
-        case 28: System.out.println("28"); break;
-        case 29: System.out.println("29"); break;
       }
     }
 
@@ -87,7 +59,7 @@ describe('instruction parser', () => {
     fileName: 'Foo',
     code: code
   });
-  const method0 = classFile.methods[1];
+  const method0 = classFile.methods[0];
 
   it('fromBytecode() == toBytecode()', () => {
     const codeAttr = getAttribute(method0, 'Code', classFile);
